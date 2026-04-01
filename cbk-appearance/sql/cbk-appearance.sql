e1b81ea2a71978682bb85e16fd9e77819bc062f4
CBK <132313680+CowBoyKeno@users.noreply.github.com>
2026-03-31 23:09:24 -0600

    Delete cbk-appearance directory

--- cbk-appearance/sql/cbk-appearance.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `cbk_appearance` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `player_identifier` VARCHAR(80) NOT NULL,
-  `appearance_json` LONGTEXT NOT NULL,
-  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ux_sa_appearance_player_identifier` (`player_identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
